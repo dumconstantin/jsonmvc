09fa3f3d170454e243bdddb7f09bf2d8ddf8d1f9
dumconstantin <dum.constantin@gmail.com>
2016-07-19 18:18:45 +0300

    Implementing patch regex

--- src/Patch.purs
+++ src/Patch.purs
@@ -1,4 +1,9 @@
-module Patch ( add ) where
+module Patch ( addPatch ) where
+
+import Data.String.Regex
+import Data.Maybe
+import Data.Either
+import Prelude
 
 type ValuePatch =
   {
@@ -7,10 +12,15 @@ type ValuePatch =
   , value :: String
   }
 
-add :: String -> String -> ValuePatch
-add path value =
-  ({
+pathReg = regex "^(/[a-zA-Z0-9]*)+$" noFlags
+
+isPatch :: String -> Either String Boolean
+isPatch x = flip test x <$> pathReg
+
+addPatch :: String -> String -> Maybe ValuePatch
+addPatch path value = if true then
+  Just ({
     op: "add"
   , path: path
   , value: value
-  })
+  }) else Nothing

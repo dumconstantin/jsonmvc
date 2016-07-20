11108ba618837b9e1e6f9bce41ed0ee5aad4db31
dumconstantin <dum.constantin@gmail.com>
2016-07-20 16:52:39 +0300

    Further implement patch

--- src/Patch.purs
+++ src/Patch.purs
@@ -1,31 +1,48 @@
-module Patch ( addPatch ) where
+module Patch ( addPatch, ValuePatch ) where
 
+import Prelude
 import Data.String.Regex
 import Data.Maybe
 import Data.Either
-import Prelude
+import Partial.Unsafe
 
-type ValuePatch =
+newtype ValuePatch = ValuePatch
   {
     op :: String
   , path :: String
   , value :: String
   }
 
-pathReg = regex "^(/[a-zA-Z0-9]*)+$" noFlags
+newtype MovePatch = MovePatch
+  {
+    op :: String
+  , path :: String
+  , from :: String
+  }
+
+newtype RemovePatch = RemovePatch
+  {
+    op :: String
+  , path :: String
+  }
 
 isPath :: String -> Boolean
-isPath x = map (\y -> test y x) pathReg
+isPath x = test y x where
+  y = unsafePartial
+    case regex "^(\\/[a-z0-9~\\-%^|\"\\ ]*)*$" noFlags of
+      Right r -> r
 
 isValue :: forall a. a -> Boolean
 isValue x = true
 
 addPatch :: String -> String -> Either String ValuePatch
-addPatch x y
-  | isPath x /= true = Left "The path is not valid"
-  | isValue y /= true = Left "The value is not valid"
-  | otherwise = Right ({
-    op: "add"
-  , path: x
-  , value: y
-  })
+addPatch path value
+  | isPath path /= true = Left ("The path '" <> path <> "' is not valid")
+  | isValue value /= true = Left ("The value '" <> value <> "' is not valid")
+  | otherwise = Right (ValuePatch { op, path, value }) where op = "add"
+
+instance showValuePatch :: Show ValuePatch where
+  show (ValuePatch x) =
+    "{ op: " <> x.op <> ", " <>
+    " path: " <> x.path <> ", " <>
+    " value: " <> x.value <> " }"

4b19dbd59dd7e1f313a419e0e23760bb8edcc2a3
dumconstantin <dum.constantin@gmail.com>
2016-07-22 12:28:57 +0300

    Learning purescript while implementing patch

--- src/Patch.purs
+++ src/Patch.purs
@@ -1,10 +1,11 @@
-module Patch ( addPatch, ValuePatch ) where
+module Patch ( add, ValuePatch ) where
 
 import Prelude
 import Data.String.Regex
 import Data.Maybe
 import Data.Either
 import Partial.Unsafe
+import Data.Array
 
 newtype ValuePatch = ValuePatch
   {
@@ -26,23 +27,40 @@ newtype RemovePatch = RemovePatch
   , path :: String
   }
 
+data PatchValue = Maybe | String | Number
+
+data Patch = ValuePatch | MovePatch | RemovePatch
+
+contains :: forall a. a -> Array -> Boolean
+contains x xs = findIndex (\y -> x == y) xs
+
+newPatch :: forall a. String -> String -> a -> Patch
+newPatch op path value
+  | contains op ["add", "merge"] == true = ValuePatch op path value
+
 isPath :: String -> Boolean
 isPath x = test y x where
   y = unsafePartial
     case regex "^(\\/[a-z0-9~\\-%^|\"\\ ]*)*$" noFlags of
       Right r -> r
 
-isValue :: forall a. a -> Boolean
+isValue :: forall a. Maybe a -> Boolean
 isValue x = true
 
-addPatch :: String -> String -> Either String ValuePatch
-addPatch path value
+create :: String -> String -> Maybe -> Either String Patch
+create op path value
   | isPath path /= true = Left ("The path '" <> path <> "' is not valid")
-  | isValue value /= true = Left ("The value '" <> value <> "' is not valid")
-  | otherwise = Right (ValuePatch { op, path, value }) where op = "add"
+  | op /= "remove" && isValue value /= true = Left ("The value '" <> value <> "' is not valid")
+  | otherwise = Right (newPatch { op, path, value })
+
+add :: String -> String -> Either String Patch
+add path value = create path (Just value)
+
+remove path = create "remove" path Nothing
 
 instance showValuePatch :: Show ValuePatch where
   show (ValuePatch x) =
     "{ op: " <> x.op <> ", " <>
     " path: " <> x.path <> ", " <>
     " value: " <> x.value <> " }"
+

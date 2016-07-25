fcc38b7953bfb82aac8c5e9f8da23ddac3bb24e9
dumconstantin <dum.constantin@gmail.com>
2016-07-25 20:59:23 +0300

    Testing other ways in Purescript

--- src/Patch.purs
+++ src/Patch.purs
@@ -1,42 +1,21 @@
-module Patch ( add, ValuePatch ) where
+module Patch where
 
-import Prelude
-import Data.String.Regex
-import Data.Maybe
 import Data.Either
+import Data.Maybe
+import Prelude
 import Partial.Unsafe
-import Data.Array
+import Data.String.Regex
 
-newtype ValuePatch = ValuePatch
-  {
-    op :: String
-  , path :: String
-  , value :: String
-  }
+newtype Path = Path String
+newtype Value = Value String
 
-newtype MovePatch = MovePatch
-  {
-    op :: String
-  , path :: String
-  , from :: String
-  }
-
-newtype RemovePatch = RemovePatch
-  {
-    op :: String
-  , path :: String
-  }
-
-data PatchValue = Maybe | String | Number
-
-data Patch = ValuePatch | MovePatch | RemovePatch
-
-contains :: forall a. a -> Array -> Boolean
-contains x xs = findIndex (\y -> x == y) xs
-
-newPatch :: forall a. String -> String -> a -> Patch
-newPatch op path value
-  | contains op ["add", "merge"] == true = ValuePatch op path value
+data Patch =
+  Add Path Value |
+  Test Path Value |
+  Replace Path Value |
+  Move Path Path |
+  Copy Path Path |
+  Remove Path
 
 isPath :: String -> Boolean
 isPath x = test y x where
@@ -44,23 +23,40 @@ isPath x = test y x where
     case regex "^(\\/[a-z0-9~\\-%^|\"\\ ]*)*$" noFlags of
       Right r -> r
 
-isValue :: forall a. Maybe a -> Boolean
+isValue :: forall a. a -> Boolean
 isValue x = true
 
-create :: String -> String -> Maybe -> Either String Patch
-create op path value
-  | isPath path /= true = Left ("The path '" <> path <> "' is not valid")
-  | op /= "remove" && isValue value /= true = Left ("The value '" <> value <> "' is not valid")
-  | otherwise = Right (newPatch { op, path, value })
+parsePath :: String -> Either String Path
+parsePath x =
+  if isPath x /= true
+    then Left ("The path '" <> x <> "' is not valid")
+    else Right (Path x)
 
-add :: String -> String -> Either String Patch
-add path value = create path (Just value)
+parseValue :: forall a. a -> Either String a
+parseValue x =
+  if isValue x /= true
+     then Left ("The value is not valid")
+     else Right x
 
-remove path = create "remove" path Nothing
+createPatch :: forall a. Patch -> Either String Path -> Either String a -> Either String Patch
+createPatch op x y
+  | isRight x && isRight y = op x y
+  | otherwise = x
 
-instance showValuePatch :: Show ValuePatch where
-  show (ValuePatch x) =
-    "{ op: " <> x.op <> ", " <>
-    " path: " <> x.path <> ", " <>
-    " value: " <> x.value <> " }"
+add :: forall a. String -> a -> Either String Patch
+add path value = createPatch Add (parsePath path) (parseValue value)
 
+testPatch :: forall a. String -> a -> Either String Patch
+testPatch path value = createPatch Test (parsePath path) (parseValue value)
+
+replace :: forall a. String -> a -> Either String Patch
+replace path value = createPatch Test (parsePath path) (parseValue value)
+
+move :: String -> String -> Either String Patch
+move fromPath toPath = createPatch Test (parsePath fromPath) (parsePath toPath)
+
+copy :: String -> String -> Either String Patch
+copy fromPath toPath = createPatch Test (parsePath fromPath) (parsePath toPath)
+
+remove :: String -> Either String Patch
+remove path = createPatch Remove (parsePath path) Right Nothing
